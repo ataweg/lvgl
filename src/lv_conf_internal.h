17a4257c1bc3db6150d92f7f79b053e604581374
awe104 <werneraxel@gmx.de>
2023-09-03 16:20:49 +0200

    make working for lvgl_esp32_drivers develop/lvgl_7.11.0_idf_5.0

--- src/lv_conf_internal.h
+++ src/lv_conf_internal.h
@@ -58,6 +58,26 @@
 #include <stdint.h>
 
 /*====================
+   Graphical settings
+ *====================*/
+
+/* Maximal horizontal and vertical resolution to support by the library.*/
+#ifndef LV_HOR_RES_MAX
+#  ifdef CONFIG_LV_HOR_RES_MAX
+#    define LV_HOR_RES_MAX CONFIG_LV_HOR_RES_MAX
+#  else
+#    define  LV_HOR_RES_MAX          (480)
+#  endif
+#endif
+#ifndef LV_VER_RES_MAX
+#  ifdef CONFIG_LV_VER_RES_MAX
+#    define LV_VER_RES_MAX CONFIG_LV_VER_RES_MAX
+#  else
+#    define  LV_VER_RES_MAX          (320)
+#  endif
+#endif
+
+/*====================
    COLOR SETTINGS
  *====================*/
 
